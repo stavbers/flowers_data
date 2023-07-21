4fe690ac34e1596a26cedf20ffb5e3e26d42f58e
stavbers <reddevweb@gmail.com>
2023-07-21 14:46:19 +0300

    add initial files

---
+++ scipt.js
@@ -0,0 +1,276 @@
+let flowers = {
+  
+}
+
+
+let obj = {
+  'headerTop': {
+    'logo': 'href//digdogdoigjdiogj',
+    'title': {
+      'top': 'Абр-Вал',
+      'bottom': 'Фабрика реставрации'
+    },
+    'btnCall': 'заказать звонок',
+    'btnShop': {
+      'btnName': 'Shop Sharing',
+      'btnLink': 'href//ldfvldkgldgldgmv'
+    },
+    'map': {
+      'mapIcon': 'https://files.stavbers.site/couch-factory/svg/adres_stroka.svg',
+      'mapName': 'г. Харьков, Героев Харькова 220а',
+      'mapLink': 'href//dgndkgldlkgvdlnvdlvlnd'
+    },
+    'actionCall': {
+      'actionIcon': 'https://files.stavbers.site/couch-factory/svg/zvonok.svg',
+      'actionPhone': '380677681999',
+      'actionTitle': 'Звонок бесплатный'
+    }
+  },
+  'headerBotm': {
+      'menu': ['О нас','Услуги','Цены','Материалы','Контакты']
+  },
+  'section-one': {
+    'block': '#FECD4B',
+    'title': 'РАССЧИТАТЬ ЦЕНУ ПЕРЕТЯЖКИ И РЕМОНТА МЕБЕЛИ',
+    'subTitle': 'Заполните форму ниже для оценки стоимости перетяжки, обивки и ремонта мягкой мебели. Мы свяжемся с Вами для уточнения деталей, определим сроки и стоимость работ.',
+    'btn': {
+      'bgColor': 'linear-gradient(180deg, #FECD4B 0%, #A57800 100%)',
+      'text': 'Рассчитать стоимость'
+    },
+    'and': {
+      'text': 'ИЛИ',
+      'color': 'rgba(254, 205, 75, 0.42)'
+    },
+    'sendPhoto': 'ОТПРАВИТЬ ФОТО',
+    'imgBg': 'https://files.stavbers.site/couch-factory/podlojka.png',
+    'slider': {
+      'img-one': 'https://files.stavbers.site/couch-factory/variant_one.png',
+      'img-two': 'https://files.stavbers.site/couch-factory/variant_two.png'
+    }
+  },
+  'section-two': {
+    'title': 'Хотите получить скидку?',
+    'block': '#FECD4B',
+    'text': 'Перетянуть ~ обтянуть диван, кресло, стулья, кухонный уголок.Работы ~ от 490 р. за 1м.кв. Сроки от 5-ти рабочих дней.Выезд замерщика по г. Екатеринбургу ~ бесплатно! Каждому клиенту ~ 2 подушки в ПОДАРОК.При заказе с сайта скидка 10% Приём заявок круглосуточный.',
+    'btn': {
+      'bgColor': 'linear-gradient(180deg, #FECD4B 0%, #A57800 100%)',
+      'text': 'Получить скидку'
+    },
+    'bgImg': 'https://files.stavbers.site/couch-factory/podarok.png'
+  },
+  'section-three': {
+    'title': 'Наши услуги',
+    'block': '#FECD4B',
+    'services': [
+      {
+        'img': 'https://files.stavbers.site/couch-factory/svg/usluga1.svg',
+        'title': 'Перетяжка мягкой мебели',
+        'link': 'Подробнне'
+      },
+      {
+        'img': 'https://files.stavbers.site/couch-factory/svg/usluga2.svg',
+        'title': 'Замена и ремонт механизмов трансформации',
+        'link': 'Подробнне'
+      },
+      {
+        'img': 'https://files.stavbers.site/couch-factory/svg/usluga3.svg',
+        'title': 'Замена пружинных блоков',
+        'link': 'Подробнне'
+      },
+      {
+        'img': 'https://files.stavbers.site/couch-factory/svg/usluga4.svg',
+        'title': 'Изменение конструкции и дизайна',
+        'link': 'Подробнне'
+      },
+      {
+        'img': 'https://files.stavbers.site/couch-factory/svg/usluga5.svg',
+        'title': 'Ремонт каркасов мебели',
+        'link': 'Подробнне'
+      },
+      {
+        'img': 'https://files.stavbers.site/couch-factory/svg/usluga6.svg',
+        'title': 'Изготовление мебели',
+        'link': 'Подробнне'
+      },
+    ],
+    'bgPics1': 'https://files.stavbers.site/couch-factory/svg/usluga_ugolok.svg',
+    'bgPics2': 'https://files.stavbers.site/couch-factory/svg/usluga_ugolok.svg',
+    'btn': {
+      'bgColor': 'linear-gradient(180deg, #FECD4B 0%, #A57800 100%)',
+      'text': 'Нужен замерщик?'
+    },
+  },
+  'section-four': {
+    'smImg': 'https://files.stavbers.site/couch-factory/svg/strujka.svg',
+    'title': 'Наши цены',
+    'block': '#FECD4B',
+    'bgImgLeft': 'https://files.stavbers.site/couch-factory/svg/podlojka_ceni.svg',
+    'bgImgChair': 'https://files.stavbers.site/couch-factory/svg/kreslo_podlojka.svg',
+    'btn': {
+      'bgColor': 'linear-gradient(180deg, #FECD4B 0%, #A57800 100%)',
+      'text': 'Заказать'
+    },
+    'rigth': [
+      {
+        'img': 'https://files.stavbers.site/couch-factory/svg/cena1.svg',
+        'title': 'Перетяжка углового дивана',
+        'price': 'от 25.000 грн. за шт'
+      },
+      {
+        'img': 'https://files.stavbers.site/couch-factory/svg/cena2.svg',
+        'title': 'Перетяжка кресла',
+        'price': 'от 7.500 грн. за шт.'
+      },
+      {
+        'img': 'https://files.stavbers.site/couch-factory/svg/cena3.svg',
+        'title': '​Обивка прямого дивана',
+        'price': 'от 490 грн. за м2'
+      },
+      {
+        'img': 'https://files.stavbers.site/couch-factory/svg/cena4.svg',
+        'title': 'Перетяжка изголовья или кровати',
+        'price': 'от 8.000 грн за шт'
+      },
+      {
+        'img': 'https://files.stavbers.site/couch-factory/svg/cena5.svg',
+        'title': 'Перетяжка кух. уголка',
+        'price': 'от 10.000 грн за шт'
+      },
+      {
+        'img': 'https://files.stavbers.site/couch-factory/svg/cena6.svg',
+        'title': ' Перетяжка стула, пуфа, банкетки.',
+        'price': 'от 1.500 грн за шт'
+      },
+    ]
+  },
+  'section-five': {
+    'smImg': 'https://files.stavbers.site/couch-factory/svg/strujka.svg',
+    'title': 'причин купить у нас',
+    'block': '#FECD4B',
+    'list': [
+      {
+        'img': 'https://files.stavbers.site/couch-factory/svg/20let.svg',
+        'info': 'На мебельном рынке'
+      },
+      {
+        'img': 'https://files.stavbers.site/couch-factory/svg/2prichina.svg',
+        'info': 'Вызов мастера и замер БЕСПЛАТНО'
+      },
+      {
+        'img': 'https://files.stavbers.site/couch-factory/svg/3prichina.svg',
+        'info': 'Транспортировка мебели собственным транспортом'
+      },
+      {
+        'img': 'https://files.stavbers.site/couch-factory/svg/4prichina.svg',
+        'info': 'Ремонтные работы ЛЮБОЙ СЛОЖНОСТИ'
+      },
+      {
+        'img': 'https://files.stavbers.site/couch-factory/svg/5prichina.svg',
+        'info': 'Предоставляем подменную мебель'
+      },
+      {
+        'img': 'https://files.stavbers.site/couch-factory/svg/6prichina.svg',
+        'info': 'Оплата работы ПО ФАКТУ'
+      }
+    ],
+    'form-title': 'Оставить заявку',
+    'form-subt': 'Оставьте заявку на сайте и получите скидку в размере 10%',
+    'form-btn': 'linear-gradient(180deg, #FECD4B 0%, #A57800 100%)',
+    'form-check': 'Я даю свое согласие на обработку персональных данных'
+  },
+  'section-six': {
+    'title': 'Этапы работы',
+    'block': '#FECD4B',
+    'bigText': 'rgba(155, 113, 0, 0.15)',
+    'items': ['Заявка или звонок','Выезд специалиста и демонстрация образцов','Заключение договора','Бесплатный вывоз мебели в мастерскую','Перетяжка и реставрация мебели'],
+    'imgBig': 'https://files.stavbers.site/couch-factory/svg/divan-etap.svg',
+    'smImg': 'https://files.stavbers.site/couch-factory/svg/strujka.svg'
+  },
+  'sextion-seven': {
+    'title': 'Примеры работ',
+    'block': '#FECD4B',
+    'slider': ['https://files.stavbers.site/couch-factory/1.png','https://files.stavbers.site/couch-factory/3_1.png','https://files.stavbers.site/couch-factory/10_1.png','https://files.stavbers.site/couch-factory/13.png','https://files.stavbers.site/couch-factory/15.png','https://files.stavbers.site/couch-factory/19.png','https://files.stavbers.site/couch-factory/28.png','https://files.stavbers.site/couch-factory/23.png']
+  },
+  'sextion-eight': {
+    'title': 'Материалы с которыми мы работаем',
+    'block': '#FECD4B',
+    'imgTop': 'https://files.stavbers.site/couch-factory/svg/strujka.svg',
+    'subTitle':'мебельных материалов',
+    'info':'Мы сотрудничаем с лучшими производителями мебельных материалов по всему миру в том числе России, Индии и Италии. Можем предложить Вам не только эксклюзивные виды ткани и кожи, но и обширную цветовую палитру. Поможем правильно подобрать обивку под Ваш интерьер и дополнить мебель фактурой.',
+    'items': [
+      {
+        'title': 'Велюр',
+        'img': 'https://files.stavbers.site/couch-factory/1_1.png'
+      },
+      {
+        'title': 'Гобелен',
+        'img': 'https://files.stavbers.site/couch-factory/2.png'
+      },
+      {
+        'title': 'Жаккард',
+        'img': 'https://files.stavbers.site/couch-factory/3.png'
+      },
+      {
+        'title': 'Флок',
+        'img': 'https://files.stavbers.site/couch-factory/4.png'
+      },
+      {
+        'title': 'Шенилл',
+        'img': 'https://files.stavbers.site/couch-factory/5.png'
+      },
+      {
+        'title': 'Рогожка',
+        'img': 'https://files.stavbers.site/couch-factory/6.png'
+      },
+      {
+        'title': 'Экокожа',
+        'img': 'https://files.stavbers.site/couch-factory/7.png'
+      },
+      {
+        'title': 'Натуральная кожа',
+        'img': 'https://files.stavbers.site/couch-factory/8.png'
+      },
+      {
+        'title': 'Искусственная кожа',
+        'img': 'https://files.stavbers.site/couch-factory/9.png'
+      },
+      {
+        'title': 'Искусственная замша',
+        'img': 'https://files.stavbers.site/couch-factory/10.png'
+      }
+    ],
+  },
+  'section-nine': {
+      'title':'Бесплатно подберем ткани под ваш интерьер',
+      'btn': {
+        'bgColor': 'linear-gradient(180deg, #FECD4B 0%, #A57800 100%)',
+        'text': 'Вызвать дизайнера на дом'
+      },
+  },
+    'section-ten': {
+      'title': 'Изготовление мебели на заказ в Харькове',
+      'block': '#FECD4B',
+      'imgTop': 'https://files.stavbers.site/couch-factory/svg/strujka.svg',
+      'link1': 'Мягкая мебель',
+      'link2': 'Корпусная мебель',
+      'linkBg': 'linear-gradient(180deg, #FECD4B 0%, #A57800 100%)',
+      'items1': [
+        {
+          'img': '',
+          'name': '',
+        },
+      ],
+    },
+
+
+}
+
+// console.log(JSON.stringify(obj))
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  fetch('http://cf-api.stavbers.site/homepage')
+  .then(resp => resp.json())
+  .then(data=> console.log(JSON.parse(data[0].homepage)))
+  // .then(data=> console.log(data))
+});
